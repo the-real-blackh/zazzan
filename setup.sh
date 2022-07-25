65999db05e1670790224007dd6d2708d9392135c
Stephen Blackheath <you@example.com>
2022-07-25 23:27:07 +0000

    Work in progress.

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ goal app call \
     --from D7QJEHMPNGK6CVW5MZF3KT3Z5RXYPGEN2VGS6LN4VQ6ZD6T6FZR4R6CTGU \
     --app-id `cat zazzan-app-id.txt` \
     --app-arg str:setup \
-    --foreign-asset 10458941,100900625
+    --foreign-asset 100900625,10458941
 

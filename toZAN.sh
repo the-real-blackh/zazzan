15ef6282044296e00ee6458c1dfbd30da94c1387
Stephen Blackheath <you@example.com>
2022-07-26 12:28:26 +0000

    Add a check for GroupSize for toZAN.

--- toZAN.sh
+++ toZAN.sh
@@ -2,13 +2,13 @@
 goal asset send \
     --from TBZHBPL54XZBY7WFOAL37R2TMHQYFIYE2JQ5PWNR5QSY7HRNNCIECKUW5Q \
     --assetid 10458941 \
-    --amount 500000000 \
+    --amount 5000000 \
     --to `cat zazzan-addr.txt` \
     -o txn1.out
 goal app call \
     --from TBZHBPL54XZBY7WFOAL37R2TMHQYFIYE2JQ5PWNR5QSY7HRNNCIECKUW5Q \
     --app-id `cat zazzan-app-id.txt` \
-    --app-arg string:toZan \
+    --app-arg string:toZAN \
     --foreign-asset 100900625 \
     -o txn2.out
 cat txn1.out txn2.out > txns.out

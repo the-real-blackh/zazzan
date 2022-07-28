f896e095c4011877da97cdc3d9db8ba3c85fff1b
Stephen Blackheath <you@example.com>
2022-07-28 07:04:05 +0000

    toZAN looks like it's working.

--- toZAN.sh
+++ toZAN.sh
@@ -2,16 +2,17 @@
 goal asset send \
     --from TBZHBPL54XZBY7WFOAL37R2TMHQYFIYE2JQ5PWNR5QSY7HRNNCIECKUW5Q \
     --assetid 10458941 \
-    --amount 5000000 \
-    --to `cat zazzan-addr.txt` \
+    --amount 1000000 \
+    --to `cat zazzan-app-addr.txt` \
     -o txn1.out
 goal app call \
     --from TBZHBPL54XZBY7WFOAL37R2TMHQYFIYE2JQ5PWNR5QSY7HRNNCIECKUW5Q \
     --app-id `cat zazzan-app-id.txt` \
     --app-arg string:toZAN \
-    --foreign-asset 100900625 \
+    --foreign-asset 10458941,100900625 \
     -o txn2.out
 cat txn1.out txn2.out > txns.out
 goal clerk group -i txns.out -o txngroup.out
 goal clerk sign -i txngroup.out -o txngroup-signed.out
 goal clerk rawsend -f txngroup-signed.out
+rm -f txn1.out txn2.out txns.out txngroup.out txngroup-signed.out

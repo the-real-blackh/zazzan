eb737172eb173b3d05581a4a7486cb453b87b349
Stephen Blackheath <you@example.com>
2022-07-28 07:52:16 +0000

    Now we can convert back and forth between ZAN and USDC.

--- toFiat.sh
+++ toFiat.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -e
 goal asset send \
     --from TBZHBPL54XZBY7WFOAL37R2TMHQYFIYE2JQ5PWNR5QSY7HRNNCIECKUW5Q \
-    --assetid 10458941 \
+    --assetid 100900625 \
     --amount $1 \
     --to `cat zazzan-app-addr.txt` \
     -o txn1.out
 goal app call \
     --from TBZHBPL54XZBY7WFOAL37R2TMHQYFIYE2JQ5PWNR5QSY7HRNNCIECKUW5Q \
     --app-id `cat zazzan-app-id.txt` \
-    --app-arg string:toZAN \
+    --app-arg string:toFiat \
     --foreign-asset 10458941,100900625 \
     -o txn2.out
 cat txn1.out txn2.out > txns.out

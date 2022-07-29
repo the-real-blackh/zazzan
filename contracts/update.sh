e6b1bb433f9311a89929044f3e3487ac3efc709d
Stephen Blackheath <you@example.com>
2022-07-29 00:52:25 +0000

    toZAN is now working right.

--- contracts/update.sh
+++ contracts/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
+if test "$Z_ENV" = "" ; then echo "Please set Z_ENV environment variable" ; false ; fi
 goal app update \
-    --from D7QJEHMPNGK6CVW5MZF3KT3Z5RXYPGEN2VGS6LN4VQ6ZD6T6FZR4R6CTGU \
-    --app-id `cat ../zazzan-app-id.txt` \
+    --from `cat ../env/$Z_ENV/genesis-addr.txt` \
+    --app-id `cat ../env/$Z_ENV/zazzan-app-id.txt` \
     --approval-prog zazzan.teal \
-    --clear-prog zazzan-clear.teal
-
+    --clear-prog ../contracts/zazzan-clear.teal
--- toZAN.sh
+++ toZAN.sh
@@ -2,18 +2,18 @@
 if test "$Z_ENV" = "" ; then echo "Please set Z_ENV environment variable" ; false ; fi
 goal asset send \
     --from $1 \
-    --assetid `cat env/$Z_ENV/USDC-asset.txt` \
+    --assetid `cat env/$Z_ENV/USDC-asset-id.txt` \
     --amount $2 \
-    --to `cat zazzan-app-addr.txt` \
+    --to `cat env/$Z_ENV/zazzan-app-addr.txt` \
     -o txn1.out
 goal app call \
     --from $1 \
-    --app-id `cat zazzan-app-id.txt` \
+    --app-id `cat env/$Z_ENV/zazzan-app-id.txt` \
     --app-arg string:toZAN \
-    --foreign-asset `cat env/$Z_ENV/USDC-asset.txt`,`cat env/$Z_ENV/ZAN-asset.txt` \
+    --foreign-asset `cat env/$Z_ENV/USDC-asset-id.txt`,`cat env/$Z_ENV/ZAN-asset-id.txt` \
     -o txn2.out
 cat txn1.out txn2.out > txns.out
 goal clerk group -i txns.out -o txngroup.out
 goal clerk sign -i txngroup.out -o txngroup-signed.out
 goal clerk rawsend -f txngroup-signed.out
-rm -f txn1.out txn2.out txns.out txngroup.out txngroup-signed.out
+rm -f txn1.out txn2.out txns.out txngroup.out txngroup-signed.out txngroup-signed.out.rej

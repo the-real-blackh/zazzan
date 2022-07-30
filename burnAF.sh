0fd6f7980a0645d0f11e11a6f0f4ca4acda15814
Stephen Blackheath <you@example.com>
2022-07-30 03:58:30 +0000

    Phase 1 is complete.

---
+++ burnAF.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+if test "$Z_ENV" = "" ; then echo "Please set Z_ENV environment variable" ; false ; fi
+# Total supply in ZANs (we add six 0's on the end because the contracts wants microZans)
+goal app call \
+    --from `cat env/$Z_ENV/genesis-addr.txt` \
+    --app-id `cat env/$Z_ENV/zazzan-app-id.txt` \
+    --app-arg str:burnAF \
+    --foreign-asset `cat env/$Z_ENV/ZAN-asset-id.txt`,`cat env/$Z_ENV/USDC-asset-id.txt` \
+    --app-account `cat env/$Z_ENV/zazzan-admin-fund.txt`
